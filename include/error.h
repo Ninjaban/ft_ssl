27ab679bd9aaafc53f15c6a0ff726ee162d13dd1
Jonathan Carra <jcarra@student.42.fr>
2019-05-13 10:52:24 +0200

    update error.h

--- include/error.h
+++ include/error.h
@@ -6,16 +6,13 @@
 /*   By: jcarra <jcarra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 10:40:05 by jcarra            #+#    #+#             */
-/*   Updated: 2019/05/13 10:40:05 by jcarra           ###   ########.fr       */
+/*   Updated: 2019/05/13 10:42:21 by jcarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERROR_H
 # define ERROR_H
 
-# include <stdio.h>
-# include <inttypes.h>
 # include <stdint.h>
-# include <stdarg.h>
 
 #endif
